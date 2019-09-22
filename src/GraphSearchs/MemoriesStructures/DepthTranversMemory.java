0b8d433af7418dfa375e22c2f23d46a57f7907b7
islam <sadat2803@gmail.com>
2019-09-22 19:23:17 +0100

    COMMIT

---
+++ src/GraphSearchs/MemoriesStructures/DepthTranversMemory.java
@@ -0,0 +1,14 @@
+
+package GraphSearchs.MemoriesStructures;
+
+/**
+ *
+ * @author PC
+ */
+public class DepthTranversMemory extends Memory{
+
+    @Override
+    public GraphSearchs.Bases.Node getNext() {
+        return this.getLast();
+    }
+}
